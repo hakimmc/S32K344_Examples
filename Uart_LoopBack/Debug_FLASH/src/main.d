ecbdd612a67e8ea066a7d98c8ad1e9aa7ba168cf
hakimmc <acalgin598@gmail.com>
2025-01-29 16:42:39 +0300

    Uart Recv & Transmit Example are working with FREERTOS! Also added ConsoleStart.py for communication explains.

---
+++ Uart_LoopBack/Debug_FLASH/src/main.d
@@ -0,0 +1,148 @@
+src/main.o: ../src/main.c ../RTD/include/Clock_Ip.h \
+ ../RTD/include/Clock_Ip_Types.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Platform_Types.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/PlatformTypes.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler_Cfg.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/CompilerDefinition.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Clock_Ip_Cfg_Defines.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_MC_CGM.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_COMMON.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/BasicTypes.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FIRC.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SIRC.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FXOSC.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SXOSC.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_PLL.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_MC_ME.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_PRAMC.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FLASH.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_CMU_FC.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SYSTICK.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_EMIOS.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_RTC.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_CONFIGURATION_GPR.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Clock_Ip_Cfg.h \
+ ../RTD/include/Clock_Ip_Types.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcu_MemMap.h \
+ ../RTD/include/Siul2_Port_Ip.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Std_Types.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/board/Siul2_Port_Ip_Cfg.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SIUL2.h \
+ ../RTD/include/Siul2_Port_Ip_Types.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Siul2_Port_Ip_Defines.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_DCM_GPR.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Port_MemMap.h \
+ ../RTD/include/Siul2_Port_Ip_Types.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Devassert.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcal.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Soc_Ips.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/IpVersionMacros.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Reg_eSys.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/OsIf_Internal.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/OsIf_Cfg.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/OsIf_ArchCfg.h \
+ ../RTD/include/Siul2_Dio_Ip.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Siul2_Dio_Ip_Cfg.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Dio_MemMap.h \
+ ../FreeRTOS/Source/include/FreeRTOS.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/FreeRTOSConfig.h \
+ ../FreeRTOS/Source/include/projdefs.h \
+ ../FreeRTOS/Source/include/portable.h \
+ ../FreeRTOS/Source/include/deprecated_definitions.h \
+ ../FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h \
+ ../FreeRTOS/Source/include/mpu_wrappers.h \
+ ../FreeRTOS/Source/include/task.h ../FreeRTOS/Source/include/list.h \
+ ../FreeRTOS/Source/include/semphr.h ../FreeRTOS/Source/include/queue.h \
+ ../FreeRTOS/Source/include/task.h ../RTD/include/Lpuart_Uart_Ip.h \
+ ../RTD/include/Lpuart_Uart_Ip_Types.h \
+ ../RTD/include/Lpuart_Uart_Ip_HwAccess.h ../RTD/include/OsIf.h \
+ ../RTD/include/OsIf_Internal.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/BaseNXP_MemMap.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Lpuart_Uart_Ip_Defines.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcal.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_LPUART.h \
+ ../RTD/include/SchM_Uart.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Rte_MemMap.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Uart_MemMap.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Lpuart_Uart_Ip_Cfg.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Lpuart_Uart_Ip_Sa_PBcfg.h \
+ ../RTD/include/Lpuart_Uart_Ip_Types.h
+../RTD/include/Clock_Ip.h:
+../RTD/include/Clock_Ip_Types.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Platform_Types.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/PlatformTypes.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler_Cfg.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/CompilerDefinition.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Clock_Ip_Cfg_Defines.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_MC_CGM.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_COMMON.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/BasicTypes.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FIRC.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SIRC.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FXOSC.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SXOSC.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_PLL.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_MC_ME.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_PRAMC.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FLASH.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_CMU_FC.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SYSTICK.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_EMIOS.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_RTC.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_CONFIGURATION_GPR.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Clock_Ip_Cfg.h:
+../RTD/include/Clock_Ip_Types.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcu_MemMap.h:
+../RTD/include/Siul2_Port_Ip.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Std_Types.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/board/Siul2_Port_Ip_Cfg.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SIUL2.h:
+../RTD/include/Siul2_Port_Ip_Types.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Siul2_Port_Ip_Defines.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_DCM_GPR.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Port_MemMap.h:
+../RTD/include/Siul2_Port_Ip_Types.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Devassert.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcal.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Soc_Ips.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/IpVersionMacros.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Reg_eSys.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/OsIf_Internal.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/OsIf_Cfg.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/OsIf_ArchCfg.h:
+../RTD/include/Siul2_Dio_Ip.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Siul2_Dio_Ip_Cfg.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Dio_MemMap.h:
+../FreeRTOS/Source/include/FreeRTOS.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/FreeRTOSConfig.h:
+../FreeRTOS/Source/include/projdefs.h:
+../FreeRTOS/Source/include/portable.h:
+../FreeRTOS/Source/include/deprecated_definitions.h:
+../FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h:
+../FreeRTOS/Source/include/mpu_wrappers.h:
+../FreeRTOS/Source/include/task.h:
+../FreeRTOS/Source/include/list.h:
+../FreeRTOS/Source/include/semphr.h:
+../FreeRTOS/Source/include/queue.h:
+../FreeRTOS/Source/include/task.h:
+../RTD/include/Lpuart_Uart_Ip.h:
+../RTD/include/Lpuart_Uart_Ip_Types.h:
+../RTD/include/Lpuart_Uart_Ip_HwAccess.h:
+../RTD/include/OsIf.h:
+../RTD/include/OsIf_Internal.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/BaseNXP_MemMap.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Lpuart_Uart_Ip_Defines.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcal.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_LPUART.h:
+../RTD/include/SchM_Uart.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Rte_MemMap.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Uart_MemMap.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Lpuart_Uart_Ip_Cfg.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/Uart_LoopBack/generate/include/Lpuart_Uart_Ip_Sa_PBcfg.h:
+../RTD/include/Lpuart_Uart_Ip_Types.h:
---
+++ Uart_LoopBack/src/main.c
@@ -0,0 +1,178 @@
+/*==================================================================================================
+*   Project              : RTD AUTOSAR 4.7
+*   Platform             : CORTEXM
+*   Peripheral           : S32K3XX
+*   Dependencies         : none
+*
+*   Autosar Version      : 4.7.0
+*   Autosar Revision     : ASR_REL_4_7_REV_0000
+*   Autosar Conf.Variant :
+*   SW Version           : 3.0.0
+*   Build Version        : S32K3_RTD_3_0_0_D2303_ASR_REL_4_7_REV_0000_20230331
+*
+*   Copyright 2020 - 2023 NXP Semiconductors
+*
+*   NXP Confidential. This software is owned or controlled by NXP and may only be
+*   used strictly in accordance with the applicable license terms. By expressly
+*   accepting such terms or by downloading, installing, activating and/or otherwise
+*   using the software, you are agreeing that you have read, and that you agree to
+*   comply with and are bound by, such license terms. If you do not agree to be
+*   bound by the applicable license terms, then you may not retain, install,
+*   activate or otherwise use the software.
+==================================================================================================*/
+
+/**
+*   @file main.c
+*
+*   @addtogroup main_module main module documentation
+*   @{
+*/
+
+/* Including necessary configuration files. */
+#include "Clock_Ip.h"
+#include "Siul2_Port_Ip.h"
+#include "Siul2_Dio_Ip.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include "Lpuart_Uart_Ip.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+volatile int exit_code = 0;
+/* User includes */
+#define INST_LPUART1 (1U)
+
+Siul2_Dio_Ip_GpioType* GPIOC_L = PTC_L_HALF;
+Siul2_Dio_Ip_GpioType* GPIOC_H = PTC_H_HALF;
+Siul2_Dio_Ip_GpioType* GPIOA_L = PTA_L_HALF;
+Siul2_Dio_Ip_GpioType* GPIOA_H = PTA_H_HALF;
+
+Siul2_Dio_Ip_PinsChannelType GREEN_LED = 1<<1;
+Siul2_Dio_Ip_PinsChannelType RED_LED   = 1<<5; // 16+5 need to be in high register
+
+#define MAIN_PRIORITY                ( tskIDLE_PRIORITY + 2 )
+SemaphoreHandle_t sem;
+
+void RED_LED_TASK ( void *pvParameters );
+void GRN_LED_TASK ( void *pvParameters );
+void UART_TASK ( void *pvParameters );
+
+/*!
+  \brief The main function for the project.
+  \details The startup initialization sequence is the following:
+ * - startup asm routine
+ * - main()
+*/
+int main(void)
+{
+    /* Write your code here */
+	Clock_Ip_Init(&Clock_Ip_aClockConfig[0]);
+
+	Lpuart_Uart_Ip_Init(LPUART_UART_IP_INSTANCE_USING_1, &Lpuart_Uart_Ip_xHwConfigPB_1);
+
+	//Clock_Ip_Init(Clock_Ip_aClockConfig);
+	Siul2_Port_Ip_Init(NUM_OF_CONFIGURED_PINS0, g_pin_mux_InitConfigArr0);
+
+	Siul2_Dio_Ip_TogglePins(GPIOA_H, RED_LED);
+
+	vSemaphoreCreateBinary(sem);
+	configASSERT(sem != NULL);
+	xSemaphoreGive(sem);
+
+	xTaskCreate(RED_LED_TASK, "RED LED TASK", configMINIMAL_STACK_SIZE, NULL, MAIN_PRIORITY, NULL);
+	xTaskCreate(GRN_LED_TASK, "GREEN LED TASK", configMINIMAL_STACK_SIZE, NULL, MAIN_PRIORITY+1, NULL);
+	xTaskCreate(UART_TASK, "UART TASK", configMINIMAL_STACK_SIZE, NULL, MAIN_PRIORITY+2, NULL);
+	vTaskStartScheduler();
+
+    for(;;);
+}
+
+void RED_LED_TASK ( void *pvParameters )
+{
+	BaseType_t operation_status;
+	(void)pvParameters;
+
+	for(;;)
+	{
+		vTaskDelay(pdMS_TO_TICKS(1));
+		operation_status = xSemaphoreTake(sem, portMAX_DELAY);
+		configASSERT(operation_status == pdPASS);
+
+		/* App Start */
+		Siul2_Dio_Ip_TogglePins(GPIOA_H, RED_LED);
+		Siul2_Dio_Ip_TogglePins(GPIOC_L, GREEN_LED);
+		vTaskDelay(pdMS_TO_TICKS(100));
+		/* App Stop */
+
+		operation_status = xSemaphoreGive(sem);
+		configASSERT(operation_status == pdPASS);
+	}
+}
+
+void GRN_LED_TASK ( void *pvParameters )
+{
+	BaseType_t operation_status;
+	(void)pvParameters;
+
+	for(;;)
+	{
+		vTaskDelay(pdMS_TO_TICKS(1));
+        operation_status = xSemaphoreTake(sem, portMAX_DELAY);
+		configASSERT(operation_status == pdPASS);/**/
+
+		/* App Start */
+		Siul2_Dio_Ip_TogglePins(GPIOC_L, GREEN_LED);
+		Siul2_Dio_Ip_TogglePins(GPIOA_H, RED_LED);
+		vTaskDelay(pdMS_TO_TICKS(100));
+		/* App Stop */
+
+        operation_status = xSemaphoreGive(sem);
+		configASSERT(operation_status == pdPASS);/**/
+	}
+}
+
+//#define UART_SEMAPHORE_CUSTOM
+
+void UART_TASK ( void *pvParameters )
+{
+	uint8_t rxbuff[24];
+	(void)pvParameters;
+	for(int i=0;i<24;i++){
+		rxbuff[i] = '\0';
+	}
+	for(;;)
+	{
+		/* App Start */
+		/*Lpuart_Uart_Ip_AsyncSend(LPUART_UART_IP_INSTANCE_USING_1, (uint8_t*)"HELLO WORLD!", 20);*/
+		Lpuart_Uart_Ip_SyncSend(LPUART_UART_IP_INSTANCE_USING_1, (uint8_t*)"data", 4, 0xFFFF);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+		Lpuart_Uart_Ip_SyncReceive(LPUART_UART_IP_INSTANCE_USING_1, rxbuff, sizeof(rxbuff), 0xFFFFFF);
+		if(rxbuff[0] != '!')
+		{
+			Lpuart_Uart_Ip_SyncSend(LPUART_UART_IP_INSTANCE_USING_1, (uint8_t*)"ERR_DATA", 8, 0xFFFF);
+			Lpuart_Uart_Ip_FlushTxBuffer(IP_LPUART_1);
+			Lpuart_Uart_Ip_FlushRxBuffer(IP_LPUART_1);
+		}
+		else{
+			Lpuart_Uart_Ip_SyncSend(LPUART_UART_IP_INSTANCE_USING_1, rxbuff, 24, 0xFFFF);
+			Lpuart_Uart_Ip_FlushTxBuffer(IP_LPUART_1);
+			Lpuart_Uart_Ip_FlushRxBuffer(IP_LPUART_1);
+
+		}
+		for(int i=0;i<24;i++){
+			rxbuff[i] = '\0';
+		}
+		vTaskDelay(pdMS_TO_TICKS(2000));
+
+
+		/* App Stop */
+		/*
+		 * Semaphore removed! But it is working. I want to make async;
+		 */
+	}
+}
+
+/** @} */
