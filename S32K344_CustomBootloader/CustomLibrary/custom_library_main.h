c3c10180166b87c3b3e29330a679226c344003d5
hakimmc <acalgin598@gmail.com>
2025-01-31 17:00:46 +0300

    Added S32K344 Custom Bootloader. Linker file is not ok. so it wont work

---
+++ S32K344_CustomBootloader/CustomLibrary/custom_library_main.h
@@ -0,0 +1,12 @@
+/*
+ * custom_library_main.h
+ *
+ *  Created on: 31 Oca 2025
+ *      Author: fvzdr
+ */
+
+#ifndef INC_CUSTOM_LIBRARY_MAIN_H_
+#define INC_CUSTOM_LIBRARY_MAIN_H_
+
+
+#endif /* INC_CUSTOM_LIBRARY_MAIN_H_ */
