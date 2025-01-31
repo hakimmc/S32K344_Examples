15debf430c289db8dda29417e33946e271d97c5f
hakimmc <acalgin598@gmail.com>
2025-01-31 17:00:46 +0300

    Added S32K344 Custom Bootloader. Linker file is not ok. so it wont work

---
+++ S32K344_CustomBootloader/Debug_FLASH/RTD/src/C40_Ip.d
@@ -0,0 +1,51 @@
+RTD/src/C40_Ip.o: ../RTD/src/C40_Ip.c ../RTD/include/C40_Ip.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcal.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler_Cfg.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/CompilerDefinition.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Platform_Types.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/PlatformTypes.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Soc_Ips.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/IpVersionMacros.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Reg_eSys.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/OsIf_Internal.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/S32K344_CustomBootloader/generate/include/OsIf_Cfg.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/S32K344_CustomBootloader/generate/include/OsIf_ArchCfg.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SYSTICK.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_COMMON.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/BasicTypes.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/S32K344_CustomBootloader/generate/include/C40_Ip_Cfg.h \
+ ../RTD/include/OsIf.h ../RTD/include/OsIf_Internal.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/BaseNXP_MemMap.h \
+ ../RTD/include/C40_Ip_Types.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_PFLASH.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FLASH.h \
+ C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mem_43_InFls_MemMap.h
+../RTD/include/C40_Ip.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcal.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler_Cfg.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/CompilerDefinition.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Platform_Types.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/PlatformTypes.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Soc_Ips.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/IpVersionMacros.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Reg_eSys.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/OsIf_Internal.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/S32K344_CustomBootloader/generate/include/OsIf_Cfg.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/S32K344_CustomBootloader/generate/include/OsIf_ArchCfg.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SYSTICK.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_COMMON.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/BasicTypes.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/S32K344_CustomBootloader/generate/include/C40_Ip_Cfg.h:
+../RTD/include/OsIf.h:
+../RTD/include/OsIf_Internal.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/BaseNXP_MemMap.h:
+../RTD/include/C40_Ip_Types.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_PFLASH.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FLASH.h:
+C\:/NXP/S32DS.3.5/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mem_43_InFls_MemMap.h:
