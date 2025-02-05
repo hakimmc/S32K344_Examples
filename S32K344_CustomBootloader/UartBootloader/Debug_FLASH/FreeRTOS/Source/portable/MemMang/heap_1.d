88e0ef1444dec0872d2d45322052bb8159d6978e
hakimmc <acalgin598@gmail.com>
2025-02-05 17:54:14 +0300

    CustomBoot working but has problems

---
+++ S32K344_CustomBootloader/UartBootloader/Debug_FLASH/FreeRTOS/Source/portable/MemMang/heap_1.d
@@ -0,0 +1,19 @@
+FreeRTOS/Source/portable/MemMang/heap_1.o: \
+ ../FreeRTOS/Source/portable/MemMang/heap_1.c \
+ ../FreeRTOS/Source/include/FreeRTOS.h \
+ C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/UartBootloader/generate/include/FreeRTOSConfig.h \
+ ../FreeRTOS/Source/include/projdefs.h \
+ ../FreeRTOS/Source/include/portable.h \
+ ../FreeRTOS/Source/include/deprecated_definitions.h \
+ ../FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h \
+ ../FreeRTOS/Source/include/mpu_wrappers.h \
+ ../FreeRTOS/Source/include/task.h ../FreeRTOS/Source/include/list.h
+../FreeRTOS/Source/include/FreeRTOS.h:
+C\:/Users/fvzdr/OneDrive/Belgeler/Github/S32_DS_Workspace/UartBootloader/generate/include/FreeRTOSConfig.h:
+../FreeRTOS/Source/include/projdefs.h:
+../FreeRTOS/Source/include/portable.h:
+../FreeRTOS/Source/include/deprecated_definitions.h:
+../FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h:
+../FreeRTOS/Source/include/mpu_wrappers.h:
+../FreeRTOS/Source/include/task.h:
+../FreeRTOS/Source/include/list.h:
