9f2488cabf49d2f5a4e9bb1d0d35db0531842ebc
hakimmc <acalgin598@gmail.com>
2025-02-06 17:59:31 +0300

    generally working but jump to app from boot is not

--- S32K344_CustomBootloader/UartBootloader/src/main.c
+++ S32K344_CustomBootloader/UartBootloader/src/main.c
@@ -15,11 +15,11 @@
 #include <stdlib.h>
 #include "C40_Ip.h"
 
-uint8_t rxbuff[9];
-uint8_t MagicWORD[5] = {'!','O','T','T','O'};
-uint8_t SkipWORD[9] = {'!','S','K','I','P','J','U','M','P'};
-uint8_t dummy[9] = {'\0','\0','\0','\0','\0','\0','\0','\0','\0',};
-uint8_t freebyte[9] = {0,0,0,0,0,0,0,0,0};
+uint8_t rxbuff[10];
+uint8_t MagicWORD[10] = {'!','O','T','T','O','W','A','K','E','!'};
+uint8_t SkipWORD[10] = {'!','S','K','I','P','J','U','M','P','!'};
+uint8_t dummy[10] = {'\0','\0','\0','\0','\0','\0','\0','\0','\0','\0'};
+uint8_t freebyte[10] = {0,0,0,0,0,0,0,0,0,0};
 uint8_t counter;
 uint8_t boot_state = 0;
 
@@ -40,14 +40,14 @@ void PIT0_IRQn_Handler(void) { __asm("NOP"); }
 void LPUART1_IRQn_Handler(void) { __asm("NOP"); }
 void PitNotification(void) { __asm("NOP"); }
 
-uint8_t CRC_Calculator(uint8_t* input,uint8_t max_len);
+uint32_t CRC_Calculator(uint8_t* input,uint8_t max_len);
 uint8_t WriteToFlash(uint8_t* input, uint8_t max_len);
 uint8_t FlashWrite(uint32_t Addr, uint8_t* Data, uint32 Length, uint8_t MASTER_ID);
 uint8_t Comparator(uint8_t* source, uint8_t* target, uint8_t len);
-void JumpToApplication( unsigned int userSP,  unsigned int userStartup);
+void JumpToUserApplication( unsigned int userSP,  unsigned int userStartup);
 void __DISABLE_PER(void);
 
-#define ReadyMessage "!RDY"
+//#define ReadyMessage "!RDY"
 #define ErrorMessage "!ERR"
 #define NextMessage "!NXT"
 #define StartMessage "!STR"
@@ -75,15 +75,18 @@ int main(void) {
     IntCtrl_Ip_EnableIrq(LPUART1_IRQn);
 
     Siul2_Dio_Ip_TogglePins(GPIOA_H, RED_LED);
-    Lpuart_Uart_Ip_AsyncReceive(LPUART_UART_IP_INSTANCE_USING_1, rxbuff, 5);
+    Lpuart_Uart_Ip_AsyncReceive(LPUART_UART_IP_INSTANCE_USING_1, rxbuff, 10);
 
-	Lpuart_Uart_Ip_SyncSend(LPUART_UART_IP_INSTANCE_USING_1, (uint8_t*) ReadyMessage, 4, 0xFFFF);
+	Lpuart_Uart_Ip_SyncSend(LPUART_UART_IP_INSTANCE_USING_1, (uint8_t*) StartMessage, 4, 0xFFFF);
 
 	for (;;) {
-		Siul2_Dio_Ip_TogglePins(GPIOC_L, GREEN_LED);
-		for(int delay = 0; delay < 0xFF; delay++);
-		Siul2_Dio_Ip_TogglePins(GPIOA_H, RED_LED);
-		for(int delay = 0; delay < 0xFF; delay++);
+		while(!boot_state){
+			for(uint64_t delay = 0; delay < 0xFFFFFFFFFF; delay++);
+			Siul2_Dio_Ip_TogglePins(GPIOA_H, RED_LED);
+			JumpToUserApplication(*((uint32_t*)APP_ADDR_START), *((uint32_t*)(APP_ADDR_START + 4)));
+		}
+
+
 	}
 }
 
@@ -94,41 +97,45 @@ void UartRxCallback(const uint8 HwInstance, const Lpuart_Uart_Ip_EventType Event
     (void) HwInstance;
     (void) Event;
     (void) UserData;
-    if(!Comparator(rxbuff, dummy, 5))
+    if(!Comparator(rxbuff, dummy, 10))
     {
 		if (boot_state) {
-			if (Comparator(rxbuff, MagicWORD, 5)) {
+			if (Comparator(rxbuff, MagicWORD, 10)) {
 				boot_state = 0;
 				__DISABLE_PER();
-				JumpToApplication(*((uint32_t*)APP_ADDR_START), *((uint32_t*)(APP_ADDR_START + 4)));
+				while(STATUS_C40_IP_SECTOR_PROTECTED != C40_Ip_GetLock(C40_Ip_GetSectorNumberFromAddress(APP_ADDR_START+WriteIndex-4))){
+					C40_Ip_SetLock(UNLOCKED_LAST_SECTOR, FLS_MASTER_ID);
+				}
+				JumpToUserApplication(*((uint32_t*)APP_ADDR_START), *((uint32_t*)(APP_ADDR_START + 4)));
 			} else{
-				if (CRC_Calculator(rxbuff, 8) == rxbuff[8]) {
-					for(int indx = 0; indx < 8; indx++){
-						FlashData[indx] = rxbuff[indx];
+				if (CRC_Calculator(rxbuff, 9) == (uint32_t)rxbuff[9]) {
+					for(int indx = 1; indx < 9; indx++){
+						FlashData[indx-1] = rxbuff[indx];
 					}
 					FlashWrite(APP_ADDR_START+WriteIndex, FlashData, FLS_BUF_SIZE, FLS_MASTER_ID);
 					Lpuart_Uart_Ip_SyncSend(LPUART_UART_IP_INSTANCE_USING_1, (uint8_t*) NextMessage, 4, 0xFFFF);
 					WriteIndex += (FLS_BUF_SIZE);
 				}
-				else if(Comparator(rxbuff,SkipWORD,9)){
-					for(int indx = 0; indx < 8; indx++){
-						FlashData[indx] = freebyte[indx];
+				else if(Comparator(rxbuff,SkipWORD,10)){
+					for(int indx = 1; indx < 9; indx++){
+						FlashData[indx-1] = freebyte[indx-1];
 					}
 					FlashWrite(APP_ADDR_START+WriteIndex, FlashData, FLS_BUF_SIZE, FLS_MASTER_ID);
 					Lpuart_Uart_Ip_SyncSend(LPUART_UART_IP_INSTANCE_USING_1, (uint8_t*) NextMessage, 4, 0xFFFF);
 					WriteIndex += (FLS_BUF_SIZE);
 				}
 			}
-
+			Siul2_Dio_Ip_TogglePins(GPIOC_L, GREEN_LED);
 		} else {
-			if (Comparator(rxbuff, MagicWORD, 5)) {
+			if (Comparator(rxbuff, MagicWORD, 10)) {
 				boot_state = 1;
 				Lpuart_Uart_Ip_SyncSend(LPUART_UART_IP_INSTANCE_USING_1, (uint8_t*) StartMessage, 4, 0xFFFF);
+				Siul2_Dio_Ip_TogglePins(GPIOC_L, GREEN_LED);
 			}
 		}
     }
-		memset(rxbuff,'\0',9);
-		Lpuart_Uart_Ip_AsyncReceive(LPUART_UART_IP_INSTANCE_USING_1, rxbuff, 9);
+		memset(rxbuff,'\0',10);
+		Lpuart_Uart_Ip_AsyncReceive(LPUART_UART_IP_INSTANCE_USING_1, rxbuff, 10);
 		IntCtrl_Ip_EnableIrq(LPUART1_IRQn);
 }
 
@@ -141,18 +148,19 @@ uint8_t Comparator(uint8_t* source, uint8_t* target, uint8_t len) {
     return 1;
 }
 
-uint8_t CRC_Calculator(uint8_t* input,uint8_t max_len)
+uint32_t CRC_Calculator(uint8_t* input,uint8_t max_len)
 {
 	uint32_t SummOfBytes = 0;
 	for(int i=0;i<max_len;i++){
 		SummOfBytes += input[i];
 	}
-	return ((uint8_t)(SummOfBytes%255));
+	return ((uint32_t)(SummOfBytes%255));
 }
 
 uint8_t FlashWrite(uint32_t Addr, uint8_t* Data, uint32 Length, uint8_t MASTER_ID)
 {
 	C40_Ip_StatusType c40Status;
+
 
 	/* Flash Lock */
 	if(UNLOCKED_LAST_SECTOR < (uint32_t)C40_Ip_GetSectorNumberFromAddress(Addr))
@@ -174,6 +182,7 @@ uint8_t FlashWrite(uint32_t Addr, uint8_t* Data, uint32 Length, uint8_t MASTER_I
 	/* Erase Flash */
 	if(C40_Ip_GetSectorNumberFromAddress(Addr) != ERASED_LAST_SECTOR)
 	{
+	    C40_Ip_MainInterfaceSectorErase(C40_Ip_GetSectorNumberFromAddress(Addr), FLS_MASTER_ID);
 		do
 		{
 			c40Status = C40_Ip_MainInterfaceSectorEraseStatus();
@@ -195,11 +204,10 @@ uint8_t FlashWrite(uint32_t Addr, uint8_t* Data, uint32 Length, uint8_t MASTER_I
 
 void __DISABLE_PER(void)
 {
-	IntCtrl_Ip_DisableIrq(LPUART1_IRQn);
-	Lpuart_Uart_Ip_Deinit(LPUART_UART_IP_INSTANCE_USING_1);
+	__asm("cpsid i");
 }
 
-void JumpToApplication( unsigned int userSP,  unsigned int userStartup)
+void JumpToUserApplication( unsigned int userSP,  unsigned int userStartup)
 {
 	/* Check if Entry address is erased and return if erased */
 	if(userSP == 0xFFFFFFFF){
