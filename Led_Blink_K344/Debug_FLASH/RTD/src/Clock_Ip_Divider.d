5f95142a79ae7d5ab06018c98fc640ebc063538f
hakimmc <acalgin598@gmail.com>
2025-03-13 17:28:08 +0300

    boot tested with ottobuggernxp

---
+++ Led_Blink_K344/Debug_FLASH/RTD/src/Clock_Ip_Divider.d
@@ -0,0 +1,78 @@
+RTD/src/Clock_Ip_Divider.o: ../RTD/src/Clock_Ip_Divider.c \
+ ../RTD/include/Clock_Ip_Private.h ../RTD/include/Clock_Ip.h \
+ ../RTD/include/Clock_Ip_Types.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Platform_Types.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/PlatformTypes.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler_Cfg.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/CompilerDefinition.h \
+ C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/Clock_Ip_Cfg_Defines.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_MC_CGM.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_COMMON.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/BasicTypes.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FIRC.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SIRC.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FXOSC.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SXOSC.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_PLL.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_MC_ME.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_PRAMC.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FLASH.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_CMU_FC.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SYSTICK.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_EMIOS.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_RTC.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_CONFIGURATION_GPR.h \
+ C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/Clock_Ip_Cfg.h \
+ ../RTD/include/Clock_Ip_Types.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcu_MemMap.h \
+ ../RTD/include/Clock_Ip_Specific.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcal.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Soc_Ips.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/IpVersionMacros.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Reg_eSys.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/OsIf_Internal.h \
+ C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/OsIf_Cfg.h \
+ C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/OsIf_ArchCfg.h \
+ ../RTD/include/Clock_Ip_Derivative_005.h
+../RTD/include/Clock_Ip_Private.h:
+../RTD/include/Clock_Ip.h:
+../RTD/include/Clock_Ip_Types.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Platform_Types.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/PlatformTypes.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Compiler_Cfg.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/CompilerDefinition.h:
+C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/Clock_Ip_Cfg_Defines.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_MC_CGM.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_COMMON.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/BasicTypes.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FIRC.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SIRC.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FXOSC.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SXOSC.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_PLL.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_MC_ME.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_PRAMC.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_FLASH.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_CMU_FC.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_SYSTICK.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_EMIOS.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_RTC.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/header/S32K344_CONFIGURATION_GPR.h:
+C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/Clock_Ip_Cfg.h:
+../RTD/include/Clock_Ip_Types.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcu_MemMap.h:
+../RTD/include/Clock_Ip_Specific.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Mcal.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/StandardTypes.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Soc_Ips.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/IpVersionMacros.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/Reg_eSys.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M30I0R0/include/OsIf_Internal.h:
+C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/OsIf_Cfg.h:
+C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/OsIf_ArchCfg.h:
+../RTD/include/Clock_Ip_Derivative_005.h:
