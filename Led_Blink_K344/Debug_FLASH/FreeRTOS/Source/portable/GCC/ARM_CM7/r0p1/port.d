3d7920a89380369a715f9857bf5e827495383016
hakimmc <acalgin598@gmail.com>
2025-03-13 17:28:08 +0300

    boot tested with ottobuggernxp

---
+++ Led_Blink_K344/Debug_FLASH/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/port.d
@@ -0,0 +1,19 @@
+FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/port.o: \
+ ../FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/port.c \
+ ../FreeRTOS/Source/include/FreeRTOS.h \
+ C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/FreeRTOSConfig.h \
+ ../FreeRTOS/Source/include/projdefs.h \
+ ../FreeRTOS/Source/include/portable.h \
+ ../FreeRTOS/Source/include/deprecated_definitions.h \
+ ../FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h \
+ ../FreeRTOS/Source/include/mpu_wrappers.h \
+ ../FreeRTOS/Source/include/task.h ../FreeRTOS/Source/include/list.h
+../FreeRTOS/Source/include/FreeRTOS.h:
+C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/FreeRTOSConfig.h:
+../FreeRTOS/Source/include/projdefs.h:
+../FreeRTOS/Source/include/portable.h:
+../FreeRTOS/Source/include/deprecated_definitions.h:
+../FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h:
+../FreeRTOS/Source/include/mpu_wrappers.h:
+../FreeRTOS/Source/include/task.h:
+../FreeRTOS/Source/include/list.h:
---
+++ Led_Blink_K344/Debug_FLASH/FreeRTOS/Source/portable/MemMang/heap_1.d
@@ -0,0 +1,19 @@
+FreeRTOS/Source/portable/MemMang/heap_1.o: \
+ ../FreeRTOS/Source/portable/MemMang/heap_1.c \
+ ../FreeRTOS/Source/include/FreeRTOS.h \
+ C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/FreeRTOSConfig.h \
+ ../FreeRTOS/Source/include/projdefs.h \
+ ../FreeRTOS/Source/include/portable.h \
+ ../FreeRTOS/Source/include/deprecated_definitions.h \
+ ../FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h \
+ ../FreeRTOS/Source/include/mpu_wrappers.h \
+ ../FreeRTOS/Source/include/task.h ../FreeRTOS/Source/include/list.h
+../FreeRTOS/Source/include/FreeRTOS.h:
+C\:/Users/ARGE13/Documents/Git/S32K344_Examples/Led_Blink_K344/generate/include/FreeRTOSConfig.h:
+../FreeRTOS/Source/include/projdefs.h:
+../FreeRTOS/Source/include/portable.h:
+../FreeRTOS/Source/include/deprecated_definitions.h:
+../FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h:
+../FreeRTOS/Source/include/mpu_wrappers.h:
+../FreeRTOS/Source/include/task.h:
+../FreeRTOS/Source/include/list.h:
